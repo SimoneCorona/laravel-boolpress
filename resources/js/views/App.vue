0070ae38b331efb6618417dedceb6ad2d9cdf0f6
SimoneCorona <corona.simone93@gmail.com>
2022-07-05 17:09:46 +0200

    pre ticket

---
+++ resources/js/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Front Office</h1>
+
+    <WorkInProgress />
+</template>
+
+<script>
+import WorkInProgress from '../components/WorkInProgress.vue';
+
+export default {
+    name: 'App',
+    components: {
+        WorkInProgress
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>Ciao {{$user->name}}, benvenuto!</h1>
+@endsection
