da4620148a2cb0d51c0cfd9dee3b403cbd634ac2
SimoneCorona <corona.simone93@gmail.com>
2022-07-08 16:48:25 +0200

    pre ticket

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            'italiano',
+            'biologico',
+            'asian fusion',
+            'medio orientale',
+            'specialità regionali'
+        ];
+        foreach($tags as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($tag, '-');
+            $new_tag->save(); 
+        }
+    }
+}
